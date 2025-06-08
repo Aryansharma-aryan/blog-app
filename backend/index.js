4c77e955e6d283c1ffbed798d5c91d0a6b88accf
Aryansharma-aryan <157520977+Aryansharma-aryan@users.noreply.github.com>
2025-06-09 00:57:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ app.use(express.json());
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:5173',
-  'http://localhost:5174'
+  'http://localhost:5174',
+ ' https://blog-verse-lovat.vercel.app/'
 
 ];
 
