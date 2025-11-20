d4df8c901702f3d938d516aaa7b796bf1ef9cc28
Aryansharma-aryan <157520977+Aryansharma-aryan@users.noreply.github.com>
2025-11-20 15:29:48 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
-const URL=process.env.URL || "mongodb+srv://arsharma2951_db_user:aryan2951@cluster0.llmynou.mongodb.net/blog-app"
+
+const URL = process.env.URL || 
+"mongodb+srv://arsharma2951_db_user:aryan2951@cluster0.llmynou.mongodb.net/blog-app";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(URL);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
+    console.error(`❌ Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
